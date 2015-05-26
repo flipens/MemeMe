237e13bb55623601a07c9f5bb8d8ec339d9662fe
Felipe Lloret <flipens@MacBook-Pro.local>
2015-05-26 23:35:19 +0200

    Project structure

---
+++ MemeMe/Classes/Controllers/SentMemesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SentMemesViewController.swift
+//  MemeMe
+//
+//  Created by Felipe Lloret on 26/05/15.
+//  Copyright (c) 2015 Felipe Lloret. All rights reserved.
+//
+
+import Foundation
---
+++ MemeMe/Classes/Model/Meme.swift
@@ -0,0 +1,9 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Felipe Lloret on 26/05/15.
+//  Copyright (c) 2015 Felipe Lloret. All rights reserved.
+//
+
+import Foundation
